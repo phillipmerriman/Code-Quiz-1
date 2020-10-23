ecf0c256ee22056c6b232a7dec4ac5db2fd14983
Phillip Merriman <phillipmerriman@gmail.com>
2020-10-23 17:23:59 -0500

    still messing with high scores, but finished readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Code-Quiz-1
+A quiz app testing users knowledge of the great Harriet Tubman.
+
+## Motivation
+I wanted to make a quiz using vanilla javasacript to showcase what I've learned so far. I'm sure when I look back on this months from now, I'll see many things that can be done better, but this is what I got so far!
+
+## Screenshots
+![Screenshot of wlecome page](images/Capture.PNG?raw=true "Screenshot of wlecome page")
+
+## How to use?
+To start the quiz, click the "begin" button and read through the questions and click on the answer as soon as possible, because the score is your final time! With each incorrect answer, you lose time!
+
+© [Phillip Merriman](https://github.com/phillipmerriman/Code-Quiz-1)
--- script.js
+++ script.js
@@ -27,6 +27,8 @@ create array of questions and answers
 create score variable
 creat timer
 */
+
+// array to hold questions
 let questions = [
     {
         question: "As a small child, Harriet received a severe injury to what part of her body?",
@@ -80,8 +82,10 @@ let questions = [
     },
 ]
 
+// array to hold high scores
 let highScores= [];
 
+// query selectors and variables
 let welcome = document.querySelector("#welcome");
 let allDone = document.querySelector("#all-done");
 let start = document.querySelector("#start");
@@ -102,22 +106,29 @@ let questionNumber = 0;
 let secondsLeft = 90;
 let stopTimer = false;
 
+// event listener for start button
 start.addEventListener("click", function (e) {
     welcome.setAttribute("style", "visibility: hidden;");
     startTimer();
     renderQuestions();
 })
 
+// event listener for go back button
 goBack.addEventListener("click", function (e) {
     welcome.setAttribute("style", "visibility: visible;");
     highScorePage.setAttribute("style", "visibility: hidden:");
 })
 
+// function to handle answer button(s) click, attached via html lines 41-44
 function handleClick(answer){
     let correctAnswer = questions[questionNumber].correctAnswer;
-    // if(questionNumber < questions.length -1) {
-        questionNumber++;
-    // }
+
+    questionNumber++;
+    
+    if (questionNumber === 5) {
+        return;
+    }
+
     if(questionNumber > questions.length - 1) {
         clearQuestions();
         allDone.setAttribute("style", "visibility: visible;");
@@ -132,10 +143,11 @@ function handleClick(answer){
     }
 }
 
+// function to render questions
 function renderQuestions () {
     let currentQuestion = questions[questionNumber].question;
     let answers = questions[questionNumber].answers;
-
+    
     qNode.setAttribute("style", "visibility: visible;");
 
     for (let i = 0; i < answers.length; i++){
@@ -146,7 +158,7 @@ function renderQuestions () {
     
 }
 
-
+// clear the questions node from the page when all questions have been answered.
 function clearQuestions () {
     questionAnswers.setAttribute("style", "display: none;");
 }
@@ -156,12 +168,13 @@ let hScores = {
     score
 };
 
+// render the high scores to the page
 function renderHighScores () {
     //retrieve the last name/score
     let localName = localStorage.getItem("name");
     let localScore = localStorage.getItem("score");
     let newLiEl = document.createElement("li");
-    let higher = localScore > hScores.score;
+    let higher = localScore > highScores.slice(-1)[0];
     hScores.name = localName;
     hScores.score = localScore;
     //if they are null, return early from this function
@@ -169,6 +182,17 @@ function renderHighScores () {
         return;
     }
     
+    highScores.push(hScores.score);
+
+    // sort highScores array from lowest to highest
+    highScores.sort(function(a, b) {
+        return a - b;
+    });
+      
+    console.log(localScore);
+    // get last item of highScores array
+    console.log(highScores.slice(-1)[0]);
+
     //else set the text of name and score to newLiEl and append if score is lower or prepend if score is higher
     newLiEl.textContent = localName + " " + localScore;
     allDone.setAttribute("style", "display: none;");
@@ -178,11 +202,11 @@ function renderHighScores () {
     } else {
         highScoreOl.append(newLiEl);
     }
-    highScores.push(hScores);
     secondsLeft = 90;
     timerEl.textContent = "Time: " + secondsLeft;
 }
 
+// handles the submit score button actions
 submitScore.addEventListener("click", function (e) {
     e.preventDefault();
     let name = document.querySelector("#fname").value;
@@ -200,12 +224,11 @@ submitScore.addEventListener("click", function (e) {
 
 // clear scores button event listener
 clearScores.addEventListener("click", function (e) {
-    // highScoreOl.innerHTML("");
     e.preventDefault();
-    // window.localStorage.removeItem(highScoreOl);
-    highScoreOl.removeChild("li");
+    highScoreOl.innerHTML = "";
 })
 
+// playing around with allowing the "enter" key to submit score
 submitScore.addEventListener("keydown", function (e) {
     e.preventDefault();
     let name = document.querySelector("#fname").value;
@@ -219,7 +242,7 @@ submitScore.addEventListener("keydown", function (e) {
     renderHighScores();
 })
 
-
+// renders high scores page when user clicks on the "view high scores" button on top left of page
 function viewHighScores () {
     welcome.setAttribute("style", "visibility: hidden");
     questionAnswers.setAttribute("style", "visibility: hidden");
@@ -227,6 +250,7 @@ function viewHighScores () {
     renderHighScores();
 }
 
+// starts the timer
 function startTimer () {
     let timerInterval = setInterval(function(){
         secondsLeft--;
